da3484f0d86ddf33f788382365aa4f8d91c89147
Ayushpatel2026 <ayushpatel.2026@gmail.com>
2024-07-23 19:12:22 -0400

    completed the header (close #1)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import Button from './Button';
 import './Header.css';
 
 function Header(){
@@ -6,7 +5,7 @@ function Header(){
         console.log("Scrolling to section: ", id);
         const element = document.getElementById(id);
         if (element) { // Check if element is not null
-            const yOffset = -55; // Adjust this value according to your header height
+            const yOffset = -75; // Adjust this value according to your header height
             const yPosition = element.getBoundingClientRect().top + window.scrollY + yOffset;
             console.log(yPosition);
     
@@ -18,14 +17,31 @@ function Header(){
 
     return (
         <div className="header">
-            <div className="left-section">
-                <h1>Ayush Patel</h1>
+            <div className="logo-container">
+                <span className="header-logo-name">
+                    AYUSH PATEL
+                </span>
             </div>
-            <div className="right-section">
-                <Button onClick={() => scrollToSection('about')}><a>About</a></Button>
-                <Button onClick={() => scrollToSection('experience')}><a>Experience</a></Button>
-                <Button onClick={() => scrollToSection('projects')}><a>Projects</a></Button>
-                <Button onClick={() => scrollToSection('contact')}><a>Contact</a></Button> {/* Contact button will have different style*/}
+            <div className="tab-section">
+                <ul className="header-links">
+                    <li className="header-link-wrapper" onClick={() => scrollToSection('about')}>
+                        <a className="header-link">About</a>
+                    </li>
+                    <li className="header-link-wrapper" onClick={() => scrollToSection('experience')}>
+                        <a className="header-link">Experience</a>
+                    </li>
+                    <li className="header-link-wrapper" onClick={() => scrollToSection('projects')}>
+                        <a className="header-link">Projects</a>
+                    </li>
+                    <li className="header-link-wrapper" onClick={() => scrollToSection('contact')}>
+                        <a className="header-link">Contacts</a>
+                    </li>
+    
+                </ul>
+                {/* <Tab className="header-tab" onClick={() => scrollToSection('about')}><a className="header-link">About</a></Tab>
+                <Tab className="header-tab" onClick={() => scrollToSection('experience')}><a className="header-link">Experience</a></Tab>
+                <Tab className="header-tab" onClick={() => scrollToSection('projects')}><a className="header-link">Projects</a></Tab>
+                <Tab className="header-tab" onClick={() => scrollToSection('contact')}><a className="header-link">Contact</a></Tab> Contact button will have different style */}
             </div>
         </div>
     );
